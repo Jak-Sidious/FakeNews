1185753d3230f5463a8be7eb1dec489ecd851aa1
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-08 13:35:17 +0300

    Changes to enable Builds as well as pass of static analysis tools

--- app/src/main/java/com/example/fakenews/Data/database/SourceDatabase.kt
+++ app/src/main/java/com/example/fakenews/Data/database/SourceDatabase.kt
@@ -2,5 +2,5 @@ package com.example.fakenews.data.database
 
 //@Database(entities = [Source::class], version = 1)
 //abstract class sourceDatabase : RoomDatabase() {
-//    abstract fun SourceDao(): SourceDao
+//    abstract fun sourceDao(): sourceDao
 //}
