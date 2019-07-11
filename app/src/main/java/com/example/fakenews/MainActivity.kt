22284313be204723c876484cbf025be653d668f4
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-11 12:23:09 +0300

    Fixes to enable linting to pass

--- app/src/main/java/com/example/fakenews/MainActivity.kt
+++ app/src/main/java/com/example/fakenews/MainActivity.kt
@@ -24,6 +24,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //TODO() Setup Backup options & Firebase Indexing
     var mRecyclerView: RecyclerView? = null
     var swipeRefresh: SwipeRefreshLayout? = null
     var sourceViewModel: SourceViewModel? = null
