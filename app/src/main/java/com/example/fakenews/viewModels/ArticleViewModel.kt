82250a2b9efd9f87027873a65e4a5358698a43e6
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-11 10:34:03 +0300

    Setup test reporting and test view model

--- app/src/main/java/com/example/fakenews/viewModels/ArticleViewModel.kt
+++ app/src/main/java/com/example/fakenews/viewModels/ArticleViewModel.kt
@@ -1,13 +1,12 @@
 package com.example.fakenews.viewModels
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.fakenews.data.dataRepository.ArticlesPerSourceRepository
 import com.example.fakenews.data.models.Article
 
-class ArticleViewModel (application: Application) : AndroidViewModel(application){
+@Suppress("EmptyDefaultConstructor")
+class ArticleViewModel () : ViewModel(){
 
     val articleRepository = ArticlesPerSourceRepository()
     val allArticles: LiveData<List<Article>> get() = articleRepository.getMutableArticleData()
