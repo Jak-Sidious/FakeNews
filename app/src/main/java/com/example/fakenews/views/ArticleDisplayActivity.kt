dc7dff64d05ff1ccc004646ad2adb112ab44eb71
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-17 11:45:15 +0300

    Implement article view via webview

--- app/src/main/java/com/example/fakenews/views/ArticleDisplayActivity.kt
+++ app/src/main/java/com/example/fakenews/views/ArticleDisplayActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fakenews.R
 import kotlinx.android.synthetic.main.web_view.*
-import retrofit2.http.Url
 import timber.log.Timber
 
 class ArticleDisplayActivity : AppCompatActivity() {
