00325e1356c584f92765976a9e216c2fe7334768
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-20 14:31:45 +0300

    Repackage of main activity, and addition of supress warnings

--- app/src/main/java/com/example/fakenews/viewModels/QueriedViewModelFactory.kt
+++ app/src/main/java/com/example/fakenews/viewModels/QueriedViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.example.fakenews.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+@Suppress("UNCHECKED_CAST")
 class QueriedViewModelFactory (private val keyword: String?,
                                private val language: String?,
                                private val sortBy: String?):ViewModelProvider.NewInstanceFactory() {
