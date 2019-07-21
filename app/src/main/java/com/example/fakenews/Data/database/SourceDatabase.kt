e7bc3f09578280dbf1ec038f045cf1068a39dd35
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-21 16:47:41 +0300

    Removal of unused files and dependencies

--- app/src/main/java/com/example/fakenews/Data/database/SourceDatabase.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fakenews.data.database
-
-//@Database(entities = [Source::class], version = 1)
-//abstract class sourceDatabase : RoomDatabase() {
-//    abstract fun sourceDao(): sourceDao
-//}
