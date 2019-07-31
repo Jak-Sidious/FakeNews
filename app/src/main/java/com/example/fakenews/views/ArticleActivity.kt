69cccc4ca35c8ef5e975875943d07a6cb287979d
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-07-31 15:31:05 +0300

    Please work

--- app/src/main/java/com/example/fakenews/views/ArticleActivity.kt
+++ app/src/main/java/com/example/fakenews/views/ArticleActivity.kt
@@ -1,12 +1,9 @@
 package com.example.fakenews.views
 
-import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.view.GestureDetector
 import android.view.Gravity
-import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
